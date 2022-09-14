4a793f74548930ef018ea08cf0a04b5587634114
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2022-09-14 08:27:04 +0200

    K9format med delvisfravær + dep bumps (#177)
    * K9format med delvisfravær + dep bumps
    
    * Testfiks + json bump
    
    * Lagt til intern.nav.no-ingresser
    
    * Fjernet ingresser, ktor+gradle bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "3.2.0.2-b18c5fe"
-val k9FormatVersion = "6.0.1"
+val dusseldorfKtorVersion = "3.2.1.1-15e2e63"
+val k9FormatVersion = "6.1.5"
 val openhtmltopdfVersion = "1.0.10"
 val handlebarsVersion = "4.3.0"
 
@@ -16,7 +16,7 @@ val fuelVersion = "2.3.1"
 val mainClass = "no.nav.helse.OmsorgspengerutbetalingeSoknadProsesseringKt"
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -59,7 +59,7 @@ dependencies {
     testImplementation ( "io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
     }
-    testImplementation("org.skyscreamer:jsonassert:1.5.0")
+    testImplementation("org.skyscreamer:jsonassert:1.5.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
@@ -102,7 +102,7 @@ tasks.withType<ShadowJar> {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "7.4.2"
+    gradleVersion = "7.5.1"
 }
 
 tasks.withType<Test> {
